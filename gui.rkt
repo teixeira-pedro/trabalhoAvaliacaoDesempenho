51ae5a85c0d6e7e040b834ca515cce5297205a10
João Pedro Abreu de Souza <jp_abreu@id.uff.br>
2019-06-23 02:01:44 -0300

    Remove code text
    There's no need to code, but there's other queue parameter

--- gui.rkt
+++ gui.rkt
@@ -13,9 +13,10 @@ Aonde o codigo do nodejs ficará
 Devemos carregar códigos de exemplo 
 |#
 
+#|
 (define codigo (new editor-canvas% [label "Codigo"] [parent janela] [line-count 10] [min-width 200] [min-height 100]
                     [vert-margin 15] [ horiz-margin 15] [editor (new text%)]))
-
+|#
 
 
 #|
@@ -33,7 +34,11 @@ Taxa de chegada de clientes por unidade de tempo
 (define label2 (new message% [parent janela] [label "taxa de clientes"]))
 (define taxa-clientes (new text-field% [parent janela] [label "taxa de clientes"]))
 
+(define label3 (new message% [parent janela] [label "seed"]))
+(define seed (new text-field% [parent janela] [label "seed"]))
 
+(define label4 (new message% [parent janela] [label "maximo numero de rodadas"]))
+(define rodadas (new text-field% [parent janela] [label "rodadas"]))
 
 
 
